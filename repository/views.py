6044c57c522e1c6ab4b009bb44718ce352ec2b8d
Marcio Silva <marciosilvadrum@gmail.com>
2023-09-17 17:45:50 -0300

    populating the main card with a fake lib of information, just for test

--- repository/views.py
+++ repository/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from utils.repository.sheets import make_sheet
+
 def home(request):
-    return  render(request, 'repository/pages/home.html')
+    return  render(request, 'repository/pages/home.html', context={
+        'repository': [make_sheet() for _ in range (6)], 
+    })
 
 def sheet(request, id):
-    return  render(request, 'repository/pages/sheet-detail.html')
+    return  render(request, 'repository/pages/sheet-detail.html', context={
+        'sheet': make_sheet(),
+    })
